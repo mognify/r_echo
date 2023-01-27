fb0139cfb13579d94acf01362f019735a0dd7d17
Levi <levimogford@gmail.com>
2023-01-27 16:35:31 -0600

    Update REcho.rs

--- REcho.rs
+++ REcho.rs
@@ -1,5 +1,15 @@
+//https://docs.rs/soundio/latest/soundio/
 let snd_ctx = soundio::Context::new();
 snd_ctx.flush_events();
 
 let snd_dvc = snd_ctx.defalt_input_device().expect("No input device");
 
+if !snd_dvc.supports_layout(soundio::ChannelLayout::get_builtin(soundio::ChannelLayoutId::Stereo)) {
+    return Err("Device doesn't support stereo".to_string());
+}
+if !snd_dvc.supports_format(soundio::Format::S16LE) {
+    return Err("Device doesn't support S16LE".to_string());
+}
+if !snd_dvc.supports_sample_rate(44100) {
+    return Err("Device doesn't 44.1 kHz".to_string());
+}
